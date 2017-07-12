fed599d323b6541d5133e5528b34f75649cc110c
Teja Ambati <tambati2@illinois.edu>
2017-07-12 12:57:25 -0500

    added database

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3
 app = Flask(__name__)
 
 @app.route("/")
 def main():
-        return "Welcome!"
+        conn = sqlite3.connect('urls.db')
+        c = conn.cursor()
+        urls = [i for i in c.execute('SELECT * FROM URLS')]
+        print(urls)
+        return render_template('index.html', links=urls)
 
 if __name__ == "__main__":
-        app.run()
+        app.run(debug=True)
